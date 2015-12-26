96655cd66dc9781f0a96537794eb46ba23522936
zygimantus <zygimantus@gmail.com>
2015-12-26 18:26:01 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Zygimantus
+ *
+ * Created on October 24, 2015, 7:53 PM
+ */
+
+#include <cstdlib>
+#include "iostream"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "HELLO WORLD" << endl;
+
+    return 0;
+}
+
